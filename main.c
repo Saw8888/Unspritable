c974149170361689e18e58c26d896984d08bc497
Jigsaw <71344333+Saw8888@users.noreply.github.com>
2024-08-11 15:56:50 +0200

    Cooler demo

--- main.c
+++ main.c
@@ -1,10 +1,63 @@
 #include "src/GGD.h"
+#include <math.h>
 
-#define pixelScale 4
+#define pixelScale 1
 #define GLW 1000
 #define GLH 500
-#define SW GLW/pixelScale+2
-#define SH GLH/pixelScale+2
+#define PI 3.1415
+#define mapX 8
+#define mapY 8
+
+float pX = 10;
+float pY = 10;
+float pDeltaX;
+float pDeltaY;
+float pAngle;
+
+unsigned int map[mapY][mapX] = {{1,1,1,1,1,1,1,1},
+                                {1,0,0,0,0,0,0,1},
+                                {1,0,1,0,0,0,0,1},
+                                {1,0,0,0,0,0,0,1},
+                                {1,0,0,0,0,0,0,1},
+                                {1,0,0,0,0,0,0,1},
+                                {1,0,0,0,0,0,0,1},
+                                {1,1,1,1,1,1,1,1}};
+
+void drawMap(){
+ for(int y=0;y<mapY;y++){
+  for(int x=0;x<mapX;x++){
+   if(map[y][x] == 1){
+    DS(x*64,y*64,60,60,(pixelData){255,255,255});
+   }
+   else{
+    DS(x*64,y*64,60,60,(pixelData){100,100,100});
+   }
+  }
+ }
+}
+
+void movePlayer(){
+ if (getKey('w')){
+   pY+=pDeltaY*5;
+   pX+=pDeltaX*5;
+  }
+ if (getKey('s')){
+  pX-=pDeltaX*5;
+  pY-=pDeltaY*5;
+ }
+ if (getKey('d')){
+  pAngle+=0.05;
+  if(pAngle > 2*PI) pAngle -= 2*PI;
+  pDeltaX = cos(pAngle);
+  pDeltaY = sin(pAngle);
+ }
+ if (getKey('a')){
+  pAngle-=0.05;
+  if(pAngle < 0) pAngle += 2*PI;
+  pDeltaX = cos(pAngle);
+  pDeltaY = sin(pAngle);
+ }
+}
 
 int main(int argc, char** argv) {
  matrix specs;
@@ -15,25 +68,26 @@ int main(int argc, char** argv) {
 
  setupScreen(&dimensions);
 
- int textureWidth = dimensions.width / pixelScale; // Adjust texture size based on pixel scale
- int textureHeight = dimensions.height / pixelScale;
-
  specs.posX = 0;
  specs.posY = 0;
- specs.height = textureHeight;
- specs.width = textureWidth;
+ specs.height = GLH;
+ specs.width = GLW;
  specs.pointSize = pixelScale;
  specs.matrix = createMatrix(specs);
 
- pixelData greenColor = {0, 255, 0}; // Define green color
+ setActiveMatrix(&specs);
 
  while (!windowClosed(&dimensions)) {
-  clearBG(&specs, greenColor);
+  clearBG((pixelData){0,0,0});
 
-  updateScreen(&specs, &dimensions);
+  movePlayer();
 
+  drawMap();
+  DL(pX,pY,pX+pDeltaX*30,pY+pDeltaY*30,5,(pixelData){255,0,255});
+  DC(pX,pY,10,(pixelData){255,0,0});
+
+  updateScreen(&dimensions, &specs);
  }
-
- terminateScreen(&specs,&dimensions);
+ terminateScreen(&dimensions, &specs);
  return 0;
 }
